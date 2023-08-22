8d4f945ffd3a4ccf24343c9fd2b6113123aa7bf9
Benoit <benoit@kestra.io>
2023-08-22 22:45:50 +0200

    [conf] edit template files to adapt to the new plugin

---
+++ src/main/java/io/kestra/plugin/scripts/malloy/CLI.java
@@ -0,0 +1,90 @@
+package io.kestra.plugin.scripts.malloy;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.runners.RunContext;
+import io.kestra.plugin.scripts.exec.AbstractExecScript;
+import io.kestra.plugin.scripts.exec.scripts.models.DockerOptions;
+import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import io.kestra.plugin.scripts.exec.scripts.runners.AbstractLogConsumer;
+import io.kestra.plugin.scripts.exec.scripts.runners.CommandsWrapper;
+import io.kestra.plugin.scripts.exec.scripts.services.ScriptService;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Execute one or more Malloy commands from the Command Line Interface."
+)
+@Plugin(examples = {
+    @Example(
+        full = true,
+        title = "Create a malloy script and run the malloy-cli run command",
+        code = """
+               id: malloy
+               namespace: dev
+               
+               tasks:
+               
+                 - id: working_dir
+                   type: io.kestra.core.tasks.flows.WorkingDirectory
+                   tasks:
+                     - id: local_file
+                       type: io.kestra.core.tasks.storages.LocalFiles
+                       inputs:
+                         model.malloy: |
+                           source: my_model is table('duckdb:https://raw.githubusercontent.com/kestra-io/datasets/main/csv/Iris.csv')
+               
+                           run: my_model -> {
+                               group_by: variety
+                               aggregate:
+                                   avg_petal_width is avg(petal_width)
+                                   avg_petal_length is avg(petal_length)
+                                   avg_sepal_width is avg(sepal_width)
+                                   avg_sepal_length is avg(sepal_length)
+                           }
+               
+                     - id: run_malloy
+                       type: io.kestra.plugin.scripts.malloy.CLI
+                       commands:
+                         - malloy-cli run model.malloy
+                """
+    )
+})
+public class CLI extends AbstractExecScript {
+    @Schema(
+        title = "Docker options when using the `DOCKER` runner"
+    )
+    @PluginProperty
+    @Builder.Default
+    protected DockerOptions docker = DockerOptions.builder()
+        .image("ghcr.io/kestra-io/malloy")
+        .build();
+
+    @Schema(
+        title = "The commands to run"
+    )
+    @PluginProperty(dynamic = true)
+    protected List<String> commands;
+
+    @Override
+    public ScriptOutput run(RunContext runContext) throws Exception {
+        List<String> commandsArgs = ScriptService.scriptCommands(
+            this.interpreter,
+            this.beforeCommands,
+            this.commands
+        );
+
+        return this.commands(runContext)
+            .withCommands(commandsArgs)
+            .run();
+    }
+}
---
+++ src/test/java/io/kestra/plugin/scripts/malloy/CLITest.java
@@ -0,0 +1,55 @@
+package io.kestra.plugin.scripts.malloy;
+
+import com.google.common.collect.ImmutableMap;
+import io.kestra.core.models.executions.LogEntry;
+import io.kestra.core.queues.QueueFactoryInterface;
+import io.kestra.core.queues.QueueInterface;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+import io.kestra.core.storages.StorageInterface;
+import io.kestra.core.utils.TestsUtils;
+import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@MicronautTest
+class CLITest {
+    @Inject
+    RunContextFactory runContextFactory;
+
+    @Inject
+    StorageInterface storageInterface;
+
+    @Inject
+    @Named(QueueFactoryInterface.WORKERTASKLOG_NAMED)
+    private QueueInterface<LogEntry> logQueue;
+
+    @Test
+    void task() throws Exception {
+        List<LogEntry> logs = new ArrayList<>();
+        logQueue.receive(logs::add);
+
+        CLI bash = CLI.builder()
+            .id("unit-test")
+            .type(CLI.class.getName())
+            .commands(List.of("malloy-cli --help"))
+            .build();
+
+        RunContext runContext = TestsUtils.mockRunContext(runContextFactory, bash, ImmutableMap.of());
+        ScriptOutput run = bash.run(runContext);
+
+        assertThat(run.getExitCode(), is(0));
+        assertThat(run.getStdOutLineCount(), is(20));
+    }
+}
